089f5e049e2d3712f753dcd8127814c041b4ba4e
skeltron <gameskeletyt@gmail.com>
2023-08-10 22:42:24 +0200

    Sql push

--- Sk-Xtc/config.lua
+++ Sk-Xtc/config.lua
@@ -5,7 +5,7 @@ SK.Maxspawn = 25 -- Heveelheid er max spawnt
 SK.Distancespawn = 30 -- Hoever je in de buurt moet zijn totdat er xtc props worden ingeladen
 
 SK.Item = 'xtc' --Item dat je krijgt als je plukt
-SK.Itemcount = math.random(3, 5) -- Hoeveelheid je ontvangt is random tussen de 3 a 5
+SK.Itemcount = math.random(1, 3) -- Hoeveelheid je ontvangt is random tussen de 3 a 5
 
 SK.SpawnZones = {
 	Xtc = {coords = vector3(-1050.7428, -3507.2839, 14.1390)}, -- Coords van het Xtc plein
---
+++ items.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('xtc', 'Xtc', 2, 0, 1),
+;
