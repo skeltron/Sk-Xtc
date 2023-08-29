01bf662a84de8e91d5383404255f31562fa7d4f7
skeltron <gameskeletyt@gmail.com>
2023-08-29 14:05:48 +0200

    Protectie update

--- server/server.lua
+++ server/server.lua
@@ -1,11 +1,30 @@
+local function distancecheckfarm(src)
+    local ECoords = SK.Zones.Xtc.coords
+	local PCoords = GetEntityCoords(GetPlayerPed(src))
+	local Dist = #(PCoords-ECoords)
+	if Dist <= 90 then return true end
+end
+
+local function distancecheckverwerk(src)
+    local ECoords = SK.Zones.Verwerk.coords
+	local PCoords = GetEntityCoords(GetPlayerPed(src))
+	local Dist = #(PCoords-ECoords)
+	if Dist <= 90 then return true end
+end
+
 RegisterServerEvent('Sk-Xtc:getitem')
 AddEventHandler('Sk-Xtc:getitem', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
 
-    xPlayer.addInventoryItem(''..SK.Item..'', SK.Itemcount)
-    TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = SK.Notify.Farm})
-    sendToDiscord(SK.Farmlogs, "SK-logs", "**Naam**: " .. GetPlayerName(src) .. "\n **ID**: " ..src .. "\n **License**: " .. GetPlayerIdentifier(src) .. "\n**Drugs:** " .. SK.Item .."\n**Aantal:** " .. SK.Itemcount .."\n**Type:** Plukken", 246504)
+    if distancecheckfarm(src) then
+
+        xPlayer.addInventoryItem(''..SK.Item..'', SK.Itemcount)
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = SK.Notify.Farm})
+        sendToDiscord(SK.Farmlogs, "SK-logs", "**Naam**: " .. GetPlayerName(src) .. "\n **ID**: " ..src .. "\n **License**: " .. GetPlayerIdentifier(src) .. "\n**Drugs:** " .. SK.Item .."\n**Aantal:** " .. SK.Itemcount .."\n**Type:** Plukken", 246504)
+    else
+        DropPlayer(src,"Event triggeren")
+    end
 end)
 
 RegisterServerEvent('Sk-Xtc:verwerkitem')
@@ -14,10 +33,16 @@ AddEventHandler('Sk-Xtc:verwerkitem', function()
     local xPlayer = ESX.GetPlayerFromId(src)
     local item = xPlayer.getInventoryItem(SK.Item).count
 
-    xPlayer.removeInventoryItem(SK.Item, SK.Removextc)
-    xPlayer.addInventoryItem(''..SK.Verwerkitem..'', SK.Verwerkcount)
-    TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = SK.Notify.Verwerk})
-    sendToDiscord(SK.Verwerklogs, "SK-logs", "**Naam**: " .. GetPlayerName(src) .. "\n **ID**: " ..src .. "\n **License**: " .. GetPlayerIdentifier(src) .. "\n**Drugs:** " .. SK.Item .."\n**Verwijder-Aantal:** " .. SK.Removextc .."\n**Drugs-Verwerk:** " .. SK.Verwerkitem .."\n**Verwerk-Aantal:** " .. SK.Verwerkcount .."\n**Type:** Verwerken", 246504)
+    if distancecheckverwerk(src) then
+
+
+        xPlayer.removeInventoryItem(SK.Item, SK.Removextc)
+        xPlayer.addInventoryItem(''..SK.Verwerkitem..'', SK.Verwerkcount)
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = SK.Notify.Verwerk})
+        sendToDiscord(SK.Verwerklogs, "SK-logs", "**Naam**: " .. GetPlayerName(src) .. "\n **ID**: " ..src .. "\n **License**: " .. GetPlayerIdentifier(src) .. "\n**Drugs:** " .. SK.Item .."\n**Verwijder-Aantal:** " .. SK.Removextc .."\n**Drugs-Verwerk:** " .. SK.Verwerkitem .."\n**Verwerk-Aantal:** " .. SK.Verwerkcount .."\n**Type:** Verwerken", 246504)
+    else
+        DropPlayer(src,"Event triggeren")
+    end
 end)
 
 ESX.RegisterServerCallback('Sk-Xtc:checkitem', function(source, cb, item)
